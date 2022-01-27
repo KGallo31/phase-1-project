b12f6c2d91b0f1c2089f8570d1d4139375e4f583
Kevin Gallo <Gallok731@gmail.com>
2022-01-27 15:30:55 -0500

    Final

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
+//Sets page with a random recipe when the page first loads
 document.addEventListener('DOMContentLoaded',() => {
   fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(r => r.json()).then(meal => setPage(meal.meals[0]))
   fetch('http://localhost:3000/meals').then(r => r.json()).then(savedMeals => savedMeals.forEach(setSavedImages))
 })
+
+//First clears the ingridents list then sets the page with a new random recipe
 document.querySelector('#newRecipe').addEventListener('click',(e) => {
-  while (ingredients.firstChild) {
-      ingredients.removeChild(ingredients.firstChild);
-  }
+  clearIngredients()
   fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(r => r.json()).then(meal => setPage(meal.meals[0]))
 })
+
+//Allows the user to add their own recipe to the page
 document.querySelector('form').addEventListener('submit',(e) => {
   e.preventDefault()
   getNewRecipe(e)
-  while (ingredients.firstChild) {
-      ingredients.removeChild(ingredients.firstChild);
-  }
+  clearIngredients()
   newIngredients(e)
 })
+
+//Allows the user to save the current shown recipe to a seperate DB
 document.querySelector('.detail-image').addEventListener('click',() => {
   const mealName = document.querySelector('#recipe-detail').querySelector('img').id
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`).then(r => r.json()).then(newObj)
 })
+
+//Global Variables
 const ingredients = document.querySelector("ul")
 const savedLocation = document.querySelector('header')
 const saved = document.createElement('div')
 savedLocation.append(saved)
+
+//This functions sets the users own recipe to the page
 function getNewRecipe(event){
   const newName = event.target["new-name"].value;
   document.querySelector('.name').textContent = newName
@@ -31,7 +38,7 @@ function getNewRecipe(event){
   ingredients.textContent = newIng
   const newDesc = event.target["new-desc"].value;
   document.querySelector('#description').textContent = newDesc
-  const newImg = event.target["new-img"].value;
+  const newImg = event.target["new-img"].value
   document.querySelector('.detail-image').src = newImg
   // console.log(document.querySelector('#recipe-detail'))
   // document.querySelector('#recipe-detail').addEventListener('click',() => 
@@ -39,6 +46,8 @@ function getNewRecipe(event){
   //   setSavedImages()
   // })
 }
+
+//This function helps format the ingridents list given by the user
 function newIngredients(event){
   const newIng = event.target['new-ingredients'].value
   const arr = newIng.split(',')
@@ -48,6 +57,8 @@ function newIngredients(event){
       ingredients.appendChild(list)
   }
 }
+
+//This function sets the page when given a recipe 
 function setPage(meal){
   const mealPic = document.querySelector('.detail-image')
       mealPic.id = meal.strMeal
@@ -70,6 +81,8 @@ function setPage(meal){
       document.querySelector('#description').textContent = desc
       document.querySelector('.name').textContent = mealName
 }
+
+//This function creates a new object that holds a recipe the user wants to save
 function newObj(meal){
   const recipeObj = {}
   const keys = Object.keys(meal.meals[0])
@@ -81,6 +94,7 @@ function newObj(meal){
   recipeObj.strMealThumb = meal.meals[0].strMealThumb
   recipeObj.strMeal = meal.meals[0].strMeal
   recipeObj.strInstructions = meal.meals[0].strInstructions
+  //This sends the object to the saved receipe DB
   fetch('http://localhost:3000/meals',  
   {
         method:'POST',
@@ -93,6 +107,8 @@ function newObj(meal){
     setSavedImages(recipeObj)
   // console.log(recipeObj)
 }
+
+//This function sets all the saved recipes to the top of the page while adding an event listener to each image
 function setSavedImages(meals){
   const savedImg = document.createElement('img')
     savedImg.src = meals.strMealThumb
@@ -100,12 +116,15 @@ function setSavedImages(meals){
     savedImg.style.width = '100px'
     savedImg.style.height = '100px'
     saved.appendChild(savedImg)
-    savedImg.addEventListener('click',() => 
-    {
-      const ingredients = document.querySelector("ul")
-      while (ingredients.firstChild) {
-        ingredients.removeChild(ingredients.firstChild);
-      }
+    savedImg.addEventListener('click',() => {
+      clearIngredients()
       setPage(meals)
     })
 } 
+
+//This function helps to clear the current ingredient list
+function clearIngredients(){
+  while (ingredients.firstChild) {
+    ingredients.removeChild(ingredients.firstChild);
+}
+}
